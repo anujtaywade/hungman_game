6aa2db186c36a664575f21db860de47716330f6b
anuj taywade <anujtaywade456@gmail.com>
2025-06-09 15:54:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-5xl text-center text-bold mt-20'>
+      Humgman game
+     </h1>
+    </>
+  )
+}
+
+export default App
