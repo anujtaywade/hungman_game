5eafe4109af4325ddba6fbe5b1932c219a8e7c2b
anuj taywade <anujtaywade456@gmail.com>
2025-06-10 02:42:20 +0530

    keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './index.css'
+import { generate, count } from "random-words";
+import { useState ,useEffect} from 'react';
 import './App.css'
-
+import Keyboard from './keyboard'
 function App() {
+const [word, setword] = useState('');
 
+useEffect(() => {
+  let generateWord=generate({minLength:6 , maxLength:7})
+  setword(generateWord)
+}, []);
 
   return (
     <>
-     <h1 className='text-4xl text-center text-bold mt-20'>
-      Humgman game
+    <Keyboard/>
+     <h1 className='text-4xl text-center font-semibold mt-20'>
+      Hungman game
+     </h1>
+     <h1>
+      The random word is {word}
      </h1>
     </>
   )
---
+++ src/keyboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Keyboard = () => {
+    let letter = Array.from({length :26 },(_,i)=>String.fromCharCode(65+i))
+    console.log(letter)
+
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Keyboard
